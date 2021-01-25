5b4206d0201d7fe45c03c2e4786f674318ef9708
phoebe <phoebe@localhost.localdomain>
2021-01-25 09:35:14 +0200

    edit in connectDB.sh

--- connectDB.sh
+++ connectDB.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+answer=""
+while [ -z $answer ]
+do	
 echo "plz enter the data base you want to connect to"
 read answer
+done
 if [ ! -d ~/DBData/$answer ]
 then
 echo "there is no database called $answer"
@@ -8,3 +12,4 @@ else
 chmod +x submenu.sh
 gnome-terminal -e "./submenu.sh $answer" >>/dev/null
 fi
+
--- insertTable.sh
+++ insertTable.sh
@@ -3,8 +3,11 @@ echo please Enter your table name
 read TableName
 if [[ -f ~/DBData/$1/$TableName ]]
 then
-	sed -n '1p' ~/DBData/$1/$TableName | awk '-F|' '{for(i=1;i<NF;i++) print $i}'	
+	sed -n '1p' ~/DBData/$1/$TableName | awk '-F|' '{for(i=1;i<NF;i++) colNameArr[i-1]=$i}'	
 else 
 echo there is no table has name $TableName
 fi
-
+for colNam in ${colNameArr[@]}
+do 
+echo $colNam
+done

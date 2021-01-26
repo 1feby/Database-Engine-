3547ff991eb452d56f489ab05997717f881f897b
Ayahassan94 <ayah7386@gmail.com>
2021-01-27 01:28:35 +0200

    update main menu

--- mainmenu.sh
+++ mainmenu.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 clear
-echo "choose what you want to do " 
-select opt in "Create Database 1" "List Database 2" "Connect To Database 3" "Drop Database " "Exit 5"
+echo "           *welcome to DBEngine*                   "
+echo "          =====================                   "  
+echo "    *choose what you want to do* "
+echo "   *----------------------------*"
+
+COLUMNS=12
+select opt in "|    Create Database 1       |" "|    List Database 2         |" "|    Connect To Database 3   |" "|    Drop Database 4         |" "|    Exit 5                  |"  "*----------------------------*"
 do
+#echo "     *-------------------------*"
 case $opt in
-"Create Database 1")source createDB.sh ;;
-"List Database 2")source ls.sh ;;
-"Connect To Database 3")source connectDB.sh ;;
-"Drop Database 4")source drop.sh ;;
-"Exit 5") exit;;
+"|    Create Database 1       |")source createDB.sh ;;
+"|    List Database 2         |")source ls.sh ;;
+"|    Connect To Database 3   |")source connectDB.sh ;;
+"|    Drop Database 4         |")source drop.sh ;;
+"|    Exit 5                  |") exit;;
+"*----------------------------*");;
+
 *) echo "Not Valid Choice";;
+#echo "   *----------------------------*"
+
 esac
 done
+
+
